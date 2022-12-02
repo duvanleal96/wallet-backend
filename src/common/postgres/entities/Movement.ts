75b0ef5545244425a21495c39ecd1e183748c635
duvan leal <duvanleal65@gmail.com>
2022-12-01 23:02:23 -0500

    fix: routes correction

--- src/common/postgres/entities/Movement.ts
+++ src/common/postgres/entities/Movement.ts
@@ -1,49 +1,49 @@
-import { Column, Entity, Index, JoinColumn, ManyToOne } from "typeorm";
-import { Account } from "./Account";
+import { Column, Entity, Index, JoinColumn, ManyToOne } from 'typeorm';
+import { Account } from './Account';
 
 @Index(
-  "movement_acc_id_income_acc_id_outcome_Idx",
-  ["accIdIncome", "accIdOutcome"],
-  {}
+  'movement_acc_id_income_acc_id_outcome_Idx',
+  ['accIdIncome', 'accIdOutcome'],
+  {},
 )
-@Index("pkmovement", ["movId"], { unique: true })
-@Entity("movement", { schema: "public" })
+@Index('pkmovement', ['movId'], { unique: true })
+@Entity('movement', { schema: 'public' })
 export class Movement {
-  @Column("uuid", { primary: true, name: "mov_id" })
+  @Column('uuid', { primary: true, name: 'mov_id' })
   movId: string;
 
-  @Column("uuid", { name: "acc_id_income" })
+  @Column('uuid', { name: 'acc_id_income' })
   accIdIncome: string;
 
-  @Column("uuid", { name: "acc_id_outcome" })
+  @Column('uuid', { name: 'acc_id_outcome' })
   accIdOutcome: string;
 
-  @Column("character varying", { name: "mov_reason", length: 500 })
+  @Column('character varying', { name: 'mov_reason', length: 500 })
   movReason: string;
 
-  @Column("bigint", { name: "mov_amount" })
+  @Column('bigint', { name: 'mov_amount' })
   movAmount: string;
 
-  @Column("integer", { name: "mov_fees", default: () => "1" })
+  @Column('integer', { name: 'mov_fees', default: () => '1' })
   movFees: number;
 
-  @Column("timestamp without time zone", {
-    name: "mov_datetime",
-    default: () => "now()",
+  @Column('timestamp without time zone', {
+    name: 'mov_datetime',
+    default: () => 'now()',
   })
   movDatetime: Date;
 
   @ManyToOne(() => Account, (account) => account.movements, {
-    onDelete: "RESTRICT",
-    onUpdate: "RESTRICT",
+    onDelete: 'RESTRICT',
+    onUpdate: 'RESTRICT',
   })
-  @JoinColumn([{ name: "acc_id_income", referencedColumnName: "accId" }])
+  @JoinColumn([{ name: 'acc_id_income', referencedColumnName: 'accId' }])
   accIdIncome2: Account;
 
   @ManyToOne(() => Account, (account) => account.movements2, {
-    onDelete: "RESTRICT",
-    onUpdate: "RESTRICT",
+    onDelete: 'RESTRICT',
+    onUpdate: 'RESTRICT',
   })
-  @JoinColumn([{ name: "acc_id_outcome", referencedColumnName: "accId" }])
+  @JoinColumn([{ name: 'acc_id_outcome', referencedColumnName: 'accId' }])
   accIdOutcome2: Account;
 }
